b36194c017e1ade8a18f95d26973d0d3fcd7c614
Lucas Sapienza <sapienzalucas@gmail.com>
2020-03-28 20:36:59 -0300

    remove giant conditionals

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,50 +6,61 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+
+      case item.name
+      when 'Aged Brie'
+        update_aged_brie(item)
+      when 'Sulfuras, Hand of Ragnaros'
+        update_sulfuras(item)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_backstage(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        update_common(item)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+
     end
+  end
+
+  def update_common(item)
+    item.sell_in -= 1
+
+    return if item.quality == 0
+
+    item.quality -=1
+  end
+
+  def update_aged_brie(item)
+    item.sell_in -= 1
+
+    return unless item.quality < 50
+
+    if item.sell_in < 0
+      item.quality += 2
+    else
+      item.quality +=1
+    end
+  end
+
+  def update_sulfuras(item)
+  end
+
+  def update_backstage(item)
+    item.sell_in -= 1
+
+    if item.sell_in < 0
+      item.quality = 0
+
+      return
+    end
+
+    if item.sell_in < 5
+      item.quality += 3
+    elsif item.sell_in < 10
+      item.quality += 2
+    else
+      item.quality += 1
+    end
+
   end
 end
 
